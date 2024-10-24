e3d8a042ba943e93e98e335db6ded3061ae03c51
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-10-24 15:18:20 +0530

    post external about section

--- about_script.js
+++ about_script.js
@@ -1,27 +1,25 @@
-// about_script.js
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Set sources for team members' photos
+    // Set sources for team members' photos (Meet the Team)
     const student1Photo = document.getElementById('student1-photo');
-    student1Photo.src = 'photos/samay.jpg'; // Update the path as necessary
+    student1Photo.src = 'photos/arm.jpeg'; // Update the path as necessary
 
     const student2Photo = document.getElementById('student2-photo');
-    student2Photo.src = 'photos/nishal.jpg'; // Ensure the file name and path are correct
+    student2Photo.src = 'photos/samay.jpg'; // Ensure the file name and path are correct
 
     const student3Photo = document.getElementById('student3-photo');
-    student3Photo.src = 'photos/urmi.jpg'; // Update the path as necessary
+    student3Photo.src = 'photos/yash.jpeg'; // Update the path as necessary
 
-    const student4Photo = document.getElementById('student4-photo');
-    student4Photo.src = 'photos/vedashree.jpeg'; // Update the path as necessary
-
-    // Set source for special thanks photo
+    // Set sources for special thanks photos
     const specialThanksPhoto1 = document.getElementById('special-thanks-photo1');
-    specialThanksPhoto1.src = 'photos/arm.jpeg'; // Update the path as necessary
+    specialThanksPhoto1.src = 'photos/nishal.jpg'; // Update the path as necessary
 
     const specialThanksPhoto2 = document.getElementById('special-thanks-photo2');
-    specialThanksPhoto2.src = 'photos/yash.jpeg'; // Update the path as necessary
+    specialThanksPhoto2.src = 'photos/urmi.jpg'; // Update the path as necessary
 
     const specialThanksPhoto3 = document.getElementById('special-thanks-photo3');
-    specialThanksPhoto3.src = 'photos/adwait.jpg'; // Update the path as necessary
+    specialThanksPhoto3.src = 'photos/vedashree.jpeg'; // Update the path as necessary
+
+    const specialThanksPhoto4 = document.getElementById('special-thanks-photo4');
+    specialThanksPhoto4.src = 'photos/adwait.jpg'; // Update the path as necessary
 });
