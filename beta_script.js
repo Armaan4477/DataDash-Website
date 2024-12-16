b31579dd65975fa7a22acbb7c5fa80f78670005a
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-12-16 19:19:30 +0530

    changed beta versions links

--- beta_script.js
+++ beta_script.js
@@ -1,3 +1,29 @@
+// Get the modal
+const modal = document.getElementById("help-modal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("help-button");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 // Function to fetch the version numbers from the JSON file and update the UI
 async function fetchVersionNumbers() {
